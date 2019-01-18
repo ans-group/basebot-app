88c72d8cbc34ed14ba600ded43f18b405763d3ae
Calvin Evans <calvin@webantic.co.uk>
2019-01-18 12:08:28 +0000

    rename config files

--- lib/config/settings.dart
+++
@@ -1,6 +0,0 @@
-class Settings {
-  static String dlSecret =
-      'pP94lStDthU.cwA.gZU.3Im6BX9RDMpAU7thYAABtcMo2-0XzWWBy6o9fRdjsQ0';
-  static String botName = 'Exie';
-  static String introText = 'Hello Human!';
-}
