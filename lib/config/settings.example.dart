1141671e2f3079ac3ec9d9602eeab27e629614de
Calvin Evans <calvin@webantic.co.uk>
2019-02-05 17:27:45 +0000

    update settings example

--- lib/config/settings.example.dart
+++ lib/config/settings.example.dart
@@ -2,5 +2,6 @@ class Settings {
   static String dlSecret = 'DIRECT_LINE_SECRET';
   static String registerURI = 'https://SOME-URL.com/register';
   static String botName = 'Basebot';
+  static String botHandle = 'basebot';
   static String introText = 'Hello Human!';
 }
