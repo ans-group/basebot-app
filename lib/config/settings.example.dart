5058da0709a7716bfa0faa022e03e8fbaf75ff32
Calvin Evans <calvin@webantic.co.uk>
2019-02-14 13:34:36 +0000

    update example handle name

--- lib/config/settings.example.dart
+++ lib/config/settings.example.dart
@@ -2,6 +2,6 @@ class Settings {
   static String dlSecret = 'DIRECT_LINE_SECRET';
   static String registerURI = 'https://SOME-URL.com/register';
   static String botName = 'Basebot';
-  static String botHandle = 'basebot';
+  static String botHandle = 'basebotHandle';
   static String introText = 'Hello Human!';
 }
